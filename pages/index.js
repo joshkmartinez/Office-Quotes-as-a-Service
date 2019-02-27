65eeb1218a78e1a3d1e300d9b787ed249da2b4dc
Joshua Martinez <joshkmartinez@gmail.com>
2019-02-26 17:23:08 -0800

    Fix FOUC

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Head from 'next/head'
 import { Grommet, Box, Heading, Button, Text, Markdown } from 'grommet'
+import withStyles from 'react-jss'
+
+const styles = {}
 const theme = {
   global: {
     colors: {
@@ -11,72 +14,73 @@ const theme = {
     }
   }
 }
-const App = () => (
-  <React.Fragment>
-    <Head>
-      <title>OQaaS</title>
-      <link
-        href="https://fonts.googleapis.com/css?family=Roboto"
-        rel="stylesheet"
-      />
-    </Head>
+class App extends React.Component {
+  componentDidMount() {}
+  render() {
+    return (
+      <React.Fragment>
+        <Head>
+          <title>OQaaS</title>
+        </Head>
+        <Grommet theme={theme}>
+          <Box
+            animation="fadeIn"
+            align="center"
+            margin="large"
+            alignContent="center"
+            background="whitesmoke"
+          >
+            <Heading margin="small">Office Quotes as a Service</Heading>
+            <Heading level={2} margin="small">
+              OQaaS
+            </Heading>
 
-    <Grommet theme={theme}>
-      <Box
-        animation="fadeIn"
-        align="center"
-        margin="large"
-        alignContent="center"
-        background="whitesmoke"
-      >
-        <Heading margin="small">Office Quotes as a Service</Heading>
-        <Heading level={2} margin="small">
-          OQaaS
-        </Heading>
+            <Markdown>{quote}</Markdown>
 
-        <Markdown>{quote}</Markdown>
+            <Button
+              label="Learn more"
+              color="brand"
+              margin="large"
+              hoverIndicator
+              primary
+              onClick={() => window.scrollTo(0, window.innerHeight)}
+            />
+          </Box>
+          <Box
+            direction="row"
+            pad="medium"
+            align="center"
+            alignContent="center"
+            animation="zoomIn"
+            basis="full"
+          >
+            <Box pad="medium" basis="1/3">
+              <Markdown>{p1}</Markdown>
+            </Box>
+            <Box pad="medium" basis="1/3">
+              <Markdown>{p2}</Markdown>
+            </Box>
+            <Box pad="medium" basis="1/3">
+              <Markdown>{p3}</Markdown>
+            </Box>
+          </Box>
+          <Box
+            pad="medium"
+            align="center"
+            alignContent="center"
+            animation="zoomIn"
+            basis="full"
+          >
+            <Box pad="medium" basis="1/2" align="center">
+              <Markdown>{example}</Markdown>
+            </Box>
+          </Box>
+        </Grommet>
+      </React.Fragment>
+    )
+  }
+}
 
-        <Button
-          label="Learn more"
-          color="brand"
-          margin="large"
-          hoverIndicator
-          primary
-          onClick={() => window.scrollTo(0, window.innerHeight)}
-        />
-      </Box>
-      <Box
-        direction="row"
-        pad="medium"
-        align="center"
-        alignContent="center"
-        animation="zoomIn"
-        basis="full"
-      >
-        <Box pad="medium" basis="1/3">
-          <Markdown>{p1}</Markdown>
-        </Box>
-        <Box pad="medium" basis="1/3">
-          <Markdown>{p2}</Markdown>
-        </Box>
-        <Box pad="medium" basis="1/3">
-          <Markdown>{p3}</Markdown>
-        </Box>
-      </Box>
-      <Box
-        pad="medium"
-        align="center"
-        alignContent="center"
-        animation="zoomIn"
-        basis="full"
-      >
-        <Box pad="medium" basis="1/2" align="center">
-          <Markdown>{example}</Markdown>
-        </Box>
-      </Box>
-    </Grommet>
-  </React.Fragment>
-)
 const quote = `
 > "Bears. Beets. Battlestar Galactica." - Jim Halpert
 `
@@ -104,4 +108,4 @@ Wanna see?
 GET oq.now.sh/api
 \`\`\`
 `
-export default App
+export default withStyles(styles)(App)
